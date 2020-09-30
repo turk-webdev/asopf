14570d3de61c2cd0f667b1ec26e2dd768999ae44
alex7090 <alexandre.de-charry@epitech.eu>
2020-09-30 06:27:56 +0200

    basic mySQL connection

--- application/routes/index.js
+++ application/routes/index.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
+// get the client
+const mysql = require('mysql2');
+
+// create the connection to database
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'azertyuiop',
+    database: 'asopf'
+});
 
 
 router.get('/', (req, res) => res.render('welcome'));
@@ -8,7 +18,14 @@ router.get('/', (req, res) => res.render('welcome'));
 router.get('/about-us/', (req, res) => res.render('about'));
 
 
-router.get('/about-us/:name', function(req, res) {
+router.get('/about-us/:name', function (req, res) {
+    connection.query(
+        'SELECT * FROM `users`',
+        function (err, results, fields) {
+            console.log(results); // results contains rows returned by server
+            console.log(fields); // fields contains extra meta data about results, if available
+        }
+    );
     var name = req.params.name;
     res.render(name);
 });
