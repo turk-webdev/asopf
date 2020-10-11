9bc431262f100fa7176fe0f249fe5542544fb05b
Nick Handy <nichandy@gmail.com>
2020-10-11 14:41:13 -0700

    Made variables of .env.example more clear

--- application/utils/database.js
+++ application/utils/database.js
@@ -1,13 +1,21 @@
 const mysql = require('mysql2');
 
+const mysqlHost = process.env.MYSQL_HOST;
+const mysqlPort = process.env.MYSQL_PORT;
+const mysqlUser = process.env.MYSQL_USER;
+const mysqlPass = process.env.MYSQL_PASS;
+const mysqlDB   = process.env.MYSQL_DB;
+
+
 // Creates a single pool with multiple available connections
 // Many connections can be open/closed simultaneously to improve efficiency
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node-complete',
-    password: 'even better than'
+    host: mysqlHost,
+    user: mysqlUser,
+    database: mysqlDB,
+    password: mysqlPass,
 });
+
 
 // Using promise chains instead of nested callbacks
 module.exports = pool.promise();
