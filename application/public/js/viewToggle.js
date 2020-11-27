9146e5be0d9223e3222befc422f2291e719db3bf
Turk Erdin <ufkun@erdin.org>
2020-11-27 12:39:49 -0800

    Broke up into separate functions to be easier to read/work with

--- application/public/js/viewToggle.js
+++ application/public/js/viewToggle.js
@@ -1,31 +1,71 @@
-function viewToggle(elem) {
-    let self;
-    let other;
-    var buttons = elem.parentNode.getElementsByTagName('button');
-    switch(elem.id.toString().toLowerCase()) {
-        case 'map':
-            self = document.getElementById('map-container');
-            other = document.getElementById('table-container');
-            self.hidden = false;
-            other.hidden = true;
-            self.className = 'col-md-12';
-            break;
-        case 'table':
-            self = document.getElementById('table-container');
-            other = document.getElementById('map-container');
-            self.hidden = false;
-            other.hidden = true;
-            self.className = 'col-md-12';
-            break;
-        default:
-            let table = document.getElementById('table-container');
-            let map = document.getElementById('map-container');
-            table.hidden = false;
-            map.hidden = false;
-            table.className = 'col-md-6';
-            map.className = 'col-md-6';
-    }
+// THIS ISN'T WORKING PROPERLY - THE DOM'S ARE COMING BACK NULL
+window.addEventListener("DOMContentLoaded",defaultView());
 
+function defaultView() {
+    console.log("test");
+    if (detectMobile()) {
+        console.log("mobile");
+        showTable();
+    } else {
+        console.log("not mobile")
+        if (innerWidth < 1500) {
+            console.log("map only");
+            showMap();
+        } else {
+            console.log("both");
+            showBoth();
+        }
+    }
+}
+
+function detectMobile() {
+    const toMatch = [
+        /Android/i,
+        /webOS/i,
+        /iPhone/i,
+        /iPad/i,
+        /iPod/i,
+        /BlackBerry/i,
+        /Windows Phone/i
+    ];
+
+    return toMatch.some((toMatchItem) => {
+        return navigator.userAgent.match(toMatchItem);
+    });
+};
+
+function showMap() {
+    let self = document.getElementById('map-container');
+    let other = document.getElementById('table-container');
+    self.style.display = 'block';
+    other.style.display = 'none';
+    self.className = 'col-md-12';
+    updateButtons();
+}
+
+function showTable() {
+    let self = document.getElementById('table-container');
+    let other = document.getElementById('map-container');
+    self.style.display = 'block';
+    other.style.display = 'none';
+    self.className = 'col-md-12';
+    updateButtons();
+}
+
+function showBoth() {
+    let myTable = document.getElementById('table-container');
+    let myMap = document.getElementById('map-container');
+    console.log(myTable);
+    console.log(myMap);
+    myTable.style.display = 'block';
+    myMap.style.display = 'block';
+    myTable.className = 'col-md-6';
+    myMap.className = 'col-md-6';
+    updateButtons();
+}
+
+function updateButtons() {
+    let buttons = document.getElementById('view-buttons').getElementsByTagName('button');
     for (let i=0; i<buttons.length; i++) {
         if (buttons[i].id.toString().toLowerCase() == elem.id.toString().toLowerCase()) {
             buttons[i].className = "btn btn-primary";
@@ -33,4 +73,20 @@ function viewToggle(elem) {
             buttons[i].className = "btn btn-secondary";
         }
     }
+    
+}
+
+function viewToggle(elem) {
+    switch(elem.id.toString().toLowerCase()) {
+        case 'map':
+            showMap();
+            break;
+        case 'table':
+            showTable();
+            break;
+        default:
+            showBoth();
+    }
+
+    updateButtons();
 }
