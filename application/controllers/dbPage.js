9e4248edeb249839602021b65a483c768816bbdb
Turk Erdin <ufkun@erdin.org>
2020-11-27 13:07:01 -0800

    Unused

--- application/controllers/dbPage.js
+++
@@ -1,8 +0,0 @@
-const Search = require('../models/search');
-
-exports.getDataForCounty = (req,res,next) => {
-    res.render('search', {
-        logged: req.user ? "yes" : "no",
-        pageTitle: 'COVID Data for'
-    });
-}
