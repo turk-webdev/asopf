3278aeaf94e202b2bcea21d4b52ecd070cf7f561
Turk Erdin <ufkun@erdin.org>
2020-11-29 21:25:53 -0800

    Split queries from Search that were more specific

---
+++ application/models/users.js
@@ -0,0 +1,20 @@
+const updateUser = (table, fname, lname, email, phone, adress, county, user_email) => {
+    var sql = "UPDATE ?? SET fname = ?, lname = ?, email = ?, phone = ?, adress = ?, county_code = ? WHERE email = ?"
+    return db.query(sql, [table, fname, lname, email, phone, adress, county, user_email]);
+};
+
+const existsUser = (table, col, query) => {
+    var sql = "SELECT EXISTS(SELECT 1 FROM ?? WHERE ?? = ?) as 'exists'";
+    return db.query(sql, [table, col, query]);
+};
+
+const insertUser = (table, email, password, role) => {
+    var sql = "INSERT INTO ?? (email, password, role) values(?, ?, ?)";
+    return db.query(sql, [table, email, password, role]);
+};
+
+module.exports = {
+    updateUser,
+    existsUser,
+    insertUser,
+};
